9a6d4b341d8eb5bda9f56bc52380c2dd0ef80c8e
Inf1nityy <br0wn_rabbit@protonmail.com>
2024-06-02 01:16:49 +0200

    good night

--- src/main/java/com/nexia/gatoanticheat/mixin/PlayerMixin.java
+++ src/main/java/com/nexia/gatoanticheat/mixin/PlayerMixin.java
@@ -1,10 +1,11 @@
 package com.nexia.gatoanticheat.mixin;
 
+import net.minecraft.world.damagesource.DamageSource;
+import net.minecraft.world.damagesource.EntityDamageSource;
 import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.LivingEntity;
-import net.minecraft.world.entity.ai.attributes.Attributes;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
 import org.spongepowered.asm.mixin.Mixin;
@@ -18,12 +19,14 @@ public abstract class PlayerMixin extends LivingEntity {
         super(entityType, level);
     }
 
-    @Inject(method = "attack", at = @At("TAIL"))
+    @Inject(method = "attack", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/Entity;hurt(Lnet/minecraft/world/damagesource/DamageSource;F)Z"))
     public void detectCriticals(Entity entity, CallbackInfo ci) {
-        double criticalHitDamage = this.getAttribute(Attributes.ATTACK_DAMAGE).getBaseValue() * 1.5F;
+        boolean bl = !this.onClimbable() && !this.isInWater() && !this.hasEffect(MobEffects.BLINDNESS) && !this.isPassenger() && entity instanceof LivingEntity;
+        boolean bl3 = bl && this.fallDistance > 0.0F && !this.onGround;
+        EntityDamageSource entityDamageSource = DamageSource.playerAttack((Player)(Object)this).setCritSpecial(bl3);
 
-        if (this.onGround && this.getAttribute(Attributes.ATTACK_DAMAGE).getValue() == criticalHitDamage) {
-            System.out.println("This player has been caught using criticals report him now");
+        if (this.onGround && entityDamageSource.isCritSpecial) {
+            System.out.println("Player using criticals");
         }
     }
 }
